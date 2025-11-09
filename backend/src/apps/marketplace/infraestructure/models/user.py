fa094b71d2981d785e340e3d71ece9d330e0307b
Luis <luisdavidmarte00@gmail.com>
2025-11-08 21:21:15 -0400

    Fixeado creacion de usuarios sin password hasheada, email requerido en la creacion del usuario

--- backend/src/apps/marketplace/infraestructure/models/user.py
+++ backend/src/apps/marketplace/infraestructure/models/user.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
             username=username,
             email=email,
             password=password
-        )
+        )        
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
