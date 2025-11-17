b6377287c912eee5567ac995fa59c3b2934fdf2a
Luis <luisdavidmarte00@gmail.com>
2025-11-16 20:34:17 -0400

    Actualizacion de la ruta de /login para tomar un nuevo view

--- backend/src/apps/marketplace/infraestructure/urls/users.py
+++ backend/src/apps/marketplace/infraestructure/urls/users.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView,TokenBlacklistView
-from src.apps.marketplace.presentation.users.views import UserViewSet,UserBalanceView
+from src.apps.marketplace.presentation.users.views import UserAuthView, UserViewSet,UserBalanceView
 from core.config.prefix import API_AUTH_PREFIX, API_BASE_PREFIX, API_USER_PREFIX
 
 router = DefaultRouter()
@@ -10,7 +10,7 @@ router.register(r'user', UserViewSet, basename='user')
 
 urlpatterns = [
     path('',include(router.urls)),
-    path(f'{API_AUTH_PREFIX}/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(f'{API_AUTH_PREFIX}/login/', UserAuthView.as_view(), name='token_obtain_pair'),
     path(f'{API_AUTH_PREFIX}/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path(f'{API_AUTH_PREFIX}/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path(f'{API_AUTH_PREFIX}/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
