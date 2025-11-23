43c5cd6ae3bc29ef29d8072e7b64812ea11eeff3
Luis David <luisdavidmarte00@gmail.com>
2025-11-22 20:49:03 -0400

    Cambios, validando necesidad de cancel item

---
+++ hardart/scripts/cancel_item.ts
@@ -0,0 +1,24 @@
+import { network } from 'hardhat';
+
+const { ethers } = await  network.connect();
+
+
+async function main() {
+  const [owner, buyer] = await ethers.getSigners();
+
+  // Conectamos al contrato ya desplegado
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const market = await Marketplace.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3");
+
+  console.log("Cancelando un producto...");
+  const txBuy = await market.connect(buyer).cancelItem(9);
+  await txBuy.wait();
+
+  const item = await market.items(9);
+  console.log(item)
+}
+
+main().catch((error) => {
+  console.error("Error:", error);
+  process.exitCode = 1;
+});
